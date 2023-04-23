feb99bba35fdc349cc893b72db2a08b680bfdbbc
HanDeul-Kim <dle1883@gmail.com>
2023-04-23 13:45:53 +0900

    setting

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className='test'>start next.js</h1>
+        </div>
+    )
+}
